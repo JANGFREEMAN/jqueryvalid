465638fa1b91455df437718e2c4bbdef08dd0db7
zhangyx <448313485@qq.com>
2016-06-13 18:22:53 +0800

    更新

--- js/jquery.validator.js
+++ js/jquery.validator.js
@@ -7,49 +7,74 @@
         this.validator(form,callBack);
     };
 
+    /**
+     *
+     * 这边定义一些内部验证规则，有新的定义规则可以在rules中精进行扩充
+     */
     Validator.rules = {
         int:/^\d+$/,
         float:/^\d+(\.\d+)?$/,
-        money:/^\d+(\.\d{2})?$/,
-        ip:"/^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$/"
+        money:/^\d+(\.\d{1,2})?$/,
+        ip:"/^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$/",
+        notempty:/^.+$/
     }
 
     Validator.msg = {
         int:"请输入整数",
         float:"请输入小数",
         money:"金额格式不正确",
-        tel:"电话号码格式不正确"
-    }
+        ip:"ip格式不正确",
+        notempty:"不能为空"
+    };
+
+    /**
+     *
+     * 这边定义一些常量
+     */
+    Validator.VALIDATOR = "validator";
+    Validator.RULE = "rule";
+    Validator.MSG = "msg";
+    Validator.REGEXP = "regexp";
+
+
 
     Validator.prototype = {
-        getValidatorInputs:function(form){
-            var inputs = form.find("input");
-            var validInput = [];
+        //获取需要校验的输入域
+        getValidatorFields:function(form){
+            var inputs = form.find("input"),
+                selects = form.find("select");
+            var validField = [];
             inputs.each(function(index,item){
                 var value = $(this).attr("validator");
                 if(value != "undefined"){
                     if(value == "true"){
-                        validInput.push($(this));
+                        validField.push($(this));
                     }
                 }
             });
-            return validInput;
+            selects.each(function(index,item){
+                var value = $(this).attr("validator");
+                if(value != "undefined"){
+                    if(value == "true"){
+                        validField.push($(this));
+                    }
+                }
+            });
+            return validField;
         },
-        validatorInput:function(input){
-            var rules = input.attr("rule") != "undefined"?input.attr("rule"):"",
+        validatorField:function(field){
+            var rules = field.attr(Validator.RULE) != "undefined"?$.trim(field.attr(Validator.RULE)):"",
                 ruleArr = rules != ""?rules.split(/\s+/):[],
                 errorMsg = [],
-                regexp = "",
-                msg = "";
+                regexp;
 
             /**
              * 验证内部验证规则
              */
-            for(i = 0 ; i < ruleArr.length; i++){
+            for(var i = 0 ; i < ruleArr.length; i++){
                 if(Validator.rules[ruleArr[i]] != ""){
-                    var regex = Validator.rules[ruleArr[i]],
-                        val = input.val();
-                    if(!regex.test(val)){
+                        regexp = Validator.rules[ruleArr[i]];
+                    if(!regexp.test(field.val())){
                         errorMsg.push(Validator.msg[ruleArr[i]]);
                     }
                 }
@@ -58,28 +83,26 @@
             /**
              * 验证自定义规则
              */
-            regexp =input.attr("regexp");
-            msg = input.attr("msg");
+            regexp = $.trim(field.attr(Validator.REGEXP));
             var pat =  new RegExp(regexp);
-            if(!pat.test(input.val())){
-                errorMsg.push(msg);
+            if(!pat.test(field.val())){
+                errorMsg.push(field.attr(Validator.MSG));
             }
             return errorMsg;
         },
         validator:function(form,callBack){
             var that = this,
-                validatorInputs = this.getValidatorInputs(form);
-            for(i = 0 ; i < validatorInputs.length ; i++){
-                var validatorInput = validatorInputs[i];
-                validatorInput.on("keyup",function(){
-                    msg =  that.validatorInput($(this));
-                    if(msg.length >0 ){
-                        callBack(validatorInput,msg);
+                validatorFields = this.getValidatorFields(form);
+            for(var i = 0 ; i < validatorFields.length ; i++){
+                var field = validatorFields[i];
+                field.on("keyup",function(){
+                    var msg =  that.validatorField($(this));
+                    if(msg.length > 0 ){
+                        callBack(field,msg);
                     }
                 });
             }
         }
-
     }
 
     Validator.init = function(forms,callBack){
@@ -87,6 +110,5 @@
            new Validator($(this),callBack);
         });
     }
-
     window["Validator"] = Validator;
 })(jQuery);
